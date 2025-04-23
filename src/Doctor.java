2504543d2bf5db3015fe862ee2b51469aa54ad49
Pacool84 <flopez_1384@hotmail.com>
2025-04-23 07:35:56 -0600

    [ADD] Creando nuestra primera clase Doctor, declarando e instanciando objetos

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    //Atributos
+    int id;
+    String name;
+    String speciality;
+
+    //Metodos ó Comportamientos
+    public void showName() {
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        Doctor myDoctor = new Doctor(); //Declarando e instanciando mi objeto
+        myDoctor.name = "Paulina López";
+        myDoctor.showName();
+    }
+}
