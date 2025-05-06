51acdbfcdddd9bd8f7db59c934c68fbf6b4e07f8
Pacool84 <flopez_1384@hotmail.com>
2025-05-05 20:32:50 -0600

    [ADD] Variables primitivas y objetos en memoria, memory stacj y memory heap

--- src/Main.java
+++ src/Main.java
@@ -26,10 +26,31 @@ public class Main {
 //
 //        System.out.println(myPatient.name = "Mateo Hernandez");
 
+//      Usando Setters y Getters
         Patient myPatient = new Patient("Brenda Rosales", "5528886165");
         myPatient.setWeight(70.5);
         myPatient.setPhoneNumber("5528886165");
         System.out.println(myPatient.getName() + " " + myPatient.getPhoneNumber() + " " + myPatient.getWeight());
+
+//      Variables primitivas y objetos en memoria
+
+        Patient myVariablePatient = new Patient("Santi Moreno", "5528886165");
+        Patient myVariablePatient2 = new Patient("Sara Moreno", "5528886165");
+
+        System.out.println("La referencia en memoria es: " + myVariablePatient);
+        System.out.println("La referencia en memoria es: " + myVariablePatient2);
+        System.out.println(myVariablePatient.getName()); // Santi Moreno
+        System.out.println(myVariablePatient2.getName()); // Sara Moreno
+
+        myVariablePatient = myVariablePatient2; //Se asigna la referencia de memoria de myVariablePatient2 a myVariablePatient
+
+        System.out.println("La referencia en memoria es: " + myVariablePatient); // Patient@65b3120a
+        System.out.println("La referencia en memoria es: " + myVariablePatient2); // Patient@65b3120a
+        System.out.println(myVariablePatient.getName()); // Sara Moreno
+        System.out.println(myVariablePatient2.getName()); // Sara Moreno
+
+
+
 
     }
 }
